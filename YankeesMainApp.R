8b65e3013de4b51c6aaf0aa3a620eaf31f55bd0c
Daniel Ledesma <46385537+dlede003@users.noreply.github.com>
2023-08-01 22:00:24 -0700

    Update YankeesMainApp.R

--- YankeesMainApp.R
+++ YankeesMainApp.R
@@ -3,9 +3,6 @@ library("shinydashboard")
 library("rsconnect")
  
 
-#rsconnect::setAccountInfo(name='daniel-ledesma-46',
-                          #token='2FD6DE6C0B95B9880BF32CC6BDBE3DBB',
-                          #secret='Ud97zPYjmx/BpYVASWRaoubppfj7+4xDZ1UBSKeS')
 
 
 # Source server and UI code
